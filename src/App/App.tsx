3ec78b568bd673deaca5708ebe6eae9949baa4fa
Aleksandar Ristevski <nice@ristevski.me>
2019-07-27 08:03:26 +0100

    Direction vs 3pieces

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -14,6 +14,7 @@ const sizes = [
     { text: 'Very Large', value: 10 },
     { text: 'Why man, why?!', value: 11 },
 ];
+const sizesLength = sizes.length - 1;
 
 interface iSelectControl {
     GridSize: number;
@@ -34,16 +35,51 @@ const SelectControl: React.SFC<iSelectControl> = (props) => (
 );
 
 const ChooserControl: React.SFC<iSelectControl> = (props) => {
-    const [index, setIndex] = React.useState<number>(8);
+    const [index, setIndex] = React.useState<number>(0);
+    const [direction, setDirection] = React.useState<number>(0);
+    const rollLeft = (size, i) => (size + (i-1)) % size;
+    const rollRight = (size, i) => (i+1) % size;
     const moveRight = () => {
-        setIndex((index+1) % 1);
+        console.log('----(+)--', index, rollRight(sizesLength, index), sizesLength);
+        setDirection(+1);
+        setIndex(rollRight(sizesLength, index));
     };
     const moveLeft = () => {
-        setIndex((index-1) % 1);
+        console.log('----(-)--', index, rollLeft(sizesLength, index), sizesLength);
+        setDirection(-1);
+        setIndex(rollLeft(sizesLength, index));
     };
-    const item = sizes[index]; 
+    const innerPiece = (item) => {
+        console.log('><<<<<', item);
+        return (
+            <div className="item" key={`k-${item.value}-${item.value}`}>
+                <div>PIXTURE XXX</div>
+                <div>{item.text} - {item.value} x {item.value}</div>
+            </div>
+        );
+    };
+    const scrollChoosenItems = (direction: number, index: number) => {
+        const item = sizes[index];
+        if (direction === 0) return innerPiece(item);
+        if (direction === -1) {
+            return [
+                innerPiece(item),
+                innerPiece(sizes[rollRight(sizesLength, index)]),
+            ];
+        } 
+        if (direction === 1) {
+            return [
+                innerPiece(sizes[rollLeft(sizesLength, index)]),
+                innerPiece(item),
+            ];
+        }
+    };
+
     return (
         <div className="Chooser">
+            <div className="Content">
+                {scrollChoosenItems(direction, index)}                
+            </div>
             <div className="ChooserWindow">
                 <div>
                     <div className="left" onClick={moveLeft} onKeyPress={moveLeft} role="button" tabIndex={-1} />
@@ -53,22 +89,8 @@ const ChooserControl: React.SFC<iSelectControl> = (props) => {
                     <div className="right" onClick={moveRight} onKeyPress={moveRight} role="button" tabIndex={-1} />
                 </div>
             </div>
-            <div className="Content" key={`k-${item.value}-${item.value}`}>
-                <div>PIXTURE XXX</div>
-                <div>{item.text} - {item.value} x {item.value}</div>   
-            </div>
             <div onClick={props.HandleChange} onKeyPress={props.HandleChange} role="button" tabIndex={0}>Choose</div>
         </div>
-    // <select onChange={props.HandleChange}>
-    //     <option key="k-0" value={0}>Please Select</option>
-    //     {sizes.map((item) => {
-    //         const optionProps = {
-    //             value: item.value,
-    //             ...(item.value === props.GridSize && { selected: true }),
-    //         };
-    //         return <option key={`k-${item.value}-${item.value}`} {...optionProps}>{item.text} - {item.value} x {item.value}</option>;
-    //     })}
-    // </select>
     );
 };
 
@@ -110,14 +132,14 @@ class App extends React.Component<MyProps, MyState> {
                     </div>
                 )}
                 {GameState && <Game GridSize={GridSize} />}
-                <div className="Panel">
+                {/* <div className="Panel">
                     <div className="Hole" />
                 </div>
                 <div className="under_scroll">
                     <p>This is some div that needs</p>
                     <p>To be scrolle</p>
                     <p>several times</p>
-                </div>
+                </div> */}
             </div>
         );
     }
